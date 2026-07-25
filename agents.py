49252799d4a5a2c80cd0fc41fbd927a92600790a
res-londres <jimuel.londres@gmail.com>
2026-07-25 20:43:24 +0800

    feat: add FoodSource agent

--- agents.py
+++ agents.py
@@ -15,3 +15,14 @@ class Queen(Ant):
     def __init__(self, model, cell):
         super().__init__(model, cell)
         self.role = 'queen'
+
+class FoodSource(CellAgent):
+    def __init__(self, model, cell, max_food, growth_rate=1):
+        super().__init__(model)
+        self.cell = cell
+        self.max_food = max_food
+        self.food_amt = max_food
+        self.growth_rate = growth_rate
+
+    def grow(self):
+        self.food_amt = min(self.max_food, self.food_amt * self.growth_rate)
